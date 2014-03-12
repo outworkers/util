0eb54496a0911cc75360dcc712e15d9745cd215c
Flavian Alexander <alexandruflavian23@gmail.com>
2014-03-12 09:25:45 +0000

    Adding unit tests for password hashing.

--- util-test/src/test/scala/com/newzly/core/PasswordHashingTest.scala
+++ util-test/src/test/scala/com/newzly/core/PasswordHashingTest.scala
@@ -1,7 +1,15 @@
 package com.newzly.core
 
+import java.util.UUID
 import org.scalatest.{ FlatSpec, Matchers }
+import com.newzly.util.core.PasswordHash
 
 class PasswordHashingTest extends FlatSpec with Matchers {
+
+  it should "correctly hash a password and decode" in {
+    val password = UUID.randomUUID().toString
+    val hashed = PasswordHash.createHash(password)
+    PasswordHash.validatePassword(password, hashed) shouldBe true
+  }
 
 }
