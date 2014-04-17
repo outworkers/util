6b4f22332801c2dccb53afff5c6f1877d99bf680
Flavian Alexander <alexandruflavian23@gmail.com>
2014-04-17 23:21:15 +0100

    Bumping version to 0.20.0

--- project/Build.scala
+++ project/Build.scala
@@ -23,7 +23,7 @@ object util extends Build {
 
 	val sharedSettings: Seq[sbt.Project.Setting[_]] = Seq(
 		organization := "com.newzly",
-		version := "0.0.19",
+		version := "0.0.20",
 		scalaVersion := "2.10.4",
 		resolvers ++= Seq(
 		"Sonatype repo"                    at "https://oss.sonatype.org/content/groups/scala-tools/",
