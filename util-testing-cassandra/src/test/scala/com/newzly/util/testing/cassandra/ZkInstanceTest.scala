1f6d04942a4f0514677d2183ed8d9a5c922c06a5
Flavian Alexandru <alexandruflavian23@gmail.com>
2014-06-26 20:50:11 +0100

    Starting the Zookeeper instance in all the tests.

--- util-testing-cassandra/src/test/scala/com/newzly/util/testing/cassandra/ZkInstanceTest.scala
+++ util-testing-cassandra/src/test/scala/com/newzly/util/testing/cassandra/ZkInstanceTest.scala
@@ -1,10 +1,14 @@
 package com.newzly.util.testing.cassandra
 
-import org.scalatest.FlatSpec
+import org.scalatest.{BeforeAndAfterAll, FlatSpec}
 import org.scalatest.matchers.ShouldMatchers
 import com.newzly.util.testing.AsyncAssertionsHelper._
 
-class ZkInstanceTest extends FlatSpec with ShouldMatchers {
+class ZkInstanceTest extends FlatSpec with ShouldMatchers with BeforeAndAfterAll {
+
+  override def beforeAll(): Unit = {
+    ZkInstance.start()
+  }
 
   it should "automatically add localhost as a default Cassandra port during test runs" in {
     val ports = ZkInstance.richClient.getData("/cassandra", watch = false)
@@ -13,5 +17,9 @@ class ZkInstanceTest extends FlatSpec with ShouldMatchers {
         new String(res.data) shouldEqual "localhost"
       }
     }
+  }
+
+  override def afterAll(): Unit = {
+    ZkInstance.stop()
   }
 }
