75cb3a093b519f61a1eaf92ce1cfc6fe2ff78c06
Flavian Alexandru <alexandruflavian23@gmail.com>
2014-06-26 20:44:24 +0100

    Upgrading cassandra test environment

--- project/Build.scala
+++ project/Build.scala
@@ -20,7 +20,7 @@ object util extends Build {
 
 	val sharedSettings: Seq[sbt.Project.Setting[_]] = Seq(
 		organization := "com.newzly",
-		version := "0.1.6",
+		version := "0.1.7",
 		scalaVersion := "2.10.4",
 		resolvers ++= Seq(
 		"Sonatype repo"                    at "https://oss.sonatype.org/content/groups/scala-tools/",
