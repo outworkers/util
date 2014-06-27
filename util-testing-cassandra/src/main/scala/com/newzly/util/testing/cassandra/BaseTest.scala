835c4f010daeb2130ffd2fb80de775237226e2e8
Flavian Alexandru <alexandruflavian23@gmail.com>
2014-06-27 21:45:52 +0100

    Adding zookeeper integration for Cassandra testing. Running Cassandra Daemon without external hlep

--- util-testing-cassandra/src/main/scala/com/newzly/util/testing/cassandra/BaseTest.scala
+++ util-testing-cassandra/src/main/scala/com/newzly/util/testing/cassandra/BaseTest.scala
@@ -1,14 +1,23 @@
 package com.newzly.util.testing.cassandra
 
-import org.cassandraunit.utils.EmbeddedCassandraServerHelper
 
-import scala.concurrent.ExecutionContext
+import java.util.concurrent.atomic.AtomicBoolean
+
+import scala.concurrent.{ blocking, ExecutionContext }
 import scala.concurrent.ExecutionContext.Implicits.global
+
+import org.cassandraunit.utils.EmbeddedCassandraServerHelper
 import org.scalatest.{ Assertions, BeforeAndAfterAll, FeatureSpec, FlatSpec, Matchers }
 import org.scalatest.concurrent.{ AsyncAssertions, ScalaFutures }
+
 import com.datastax.driver.core.{ Cluster, Session }
+import com.twitter.conversions.time._
+import com.twitter.util.Await
 
 object BaseTestHelper {
+
+  ZkInstance.start()
+  val embeddedMode = new AtomicBoolean(false)
 
   private[this] def getPort: Int = {
     if (System.getenv().containsKey("TRAVIS_JOB_ID")) {
@@ -16,37 +25,52 @@ object BaseTestHelper {
       9042
     } else {
       Console.println("Using Embedded Cassandra with port 9142")
+      embeddedMode.compareAndSet(false, true)
       9142
     }
    }
 
+  val ports = new String(Await.result(ZkInstance.richClient.getData("/cassandra", watch = false), 3.seconds).data)
+
   val cluster = Cluster.builder()
-    .addContactPoint("localhost")
+    .addContactPoint(ports)
     .withPort(getPort)
     .withoutJMXReporting()
     .withoutMetrics()
     .build()
+
+  lazy val session = blocking {
+    cluster.connect()
+  }
 }
 
 trait CassandraTest {
   self: BeforeAndAfterAll =>
+
   val keySpace: String
+
   val cluster = BaseTestHelper.cluster
-  implicit lazy val session: Session = cluster.connect()
+  implicit lazy val session: Session = BaseTestHelper.session
   implicit lazy val context: ExecutionContext = global
 
   private[this] def createKeySpace(spaceName: String) = {
-    session.execute(s"CREATE KEYSPACE IF NOT EXISTS $spaceName WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};")
-    session.execute(s"use $spaceName;")
+    blocking {
+      session.execute(s"CREATE KEYSPACE IF NOT EXISTS $spaceName WITH replication = {'class': 'SimpleStrategy', 'replication_factor' : 1};")
+      session.execute(s"use $spaceName;")
+    }
   }
+
   override def beforeAll() {
-    EmbeddedCassandraServerHelper.cleanEmbeddedCassandra()
-    EmbeddedCassandraServerHelper.startEmbeddedCassandra()
+    if (BaseTestHelper.embeddedMode.get()) {
+      EmbeddedCassandraServerHelper.startEmbeddedCassandra("embedded-cassandra.yaml")
+    }
     createKeySpace(keySpace)
   }
 
   override def afterAll() {
-    session.execute(s"DROP KEYSPACE $keySpace;")
+    blocking {
+      session.execute(s"DROP KEYSPACE $keySpace;")
+    }
   }
 
 }
