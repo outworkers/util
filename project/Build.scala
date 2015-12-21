f968297910f7493e5a8f112055bd32f20eaacfe0
Flavian Alexandru <flavian.alexandru@anomaly42.com>
2015-12-21 11:26:50 +0000

    Bumping version

--- project/Build.scala
+++ project/Build.scala
@@ -89,7 +89,7 @@ object Build extends Build {
 
   val sharedSettings: Seq[Def.Setting[_]] = Seq(
 		organization := "com.websudos",
-    version := "0.10.2",
+    version := "0.10.3",
     scalaVersion := "2.11.7",
     crossScalaVersions := Seq("2.10.5", "2.11.7"),
 		resolvers ++= Seq(
