ea334ecf15ecda13866afae4b584ae43e41dbd0f
Flavian Alexandru <flavian@outworkers.com>
2016-09-24 23:53:31 +0100

    Trying silly things

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -2,7 +2,7 @@
 if [ "${TRAVIS_SCALA_VERSION}" == "2.11.8" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
 then
     echo "Running tests with coverage and report submission"
-    sbt ++$TRAVIS_SCALA_VERSION test
+    sbt ++$TRAVIS_SCALA_VERSION coverage ++$TRAVIS_SCALA_VERSION test ++$TRAVIS_SCALA_VERSION coverageReport ++$TRAVIS_SCALA_VERSION coverageAggregate ++$TRAVIS_SCALA_VERSION coveralls
     exit $?
 else
     echo "Running tests without attempting to submit coverage reports"
