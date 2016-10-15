72c59145ef20b444b9de5a19b0ab8db06b90f8a4
Flavian Alexandru <flavian@outworkers.com>
2016-10-15 20:43:20 +0100

    Implementing automated generation of samples for more complex types.

--- util-testing/src/main/scala/com/outworkers/util/testing/DefaultSamplers.scala
+++ util-testing/src/main/scala/com/outworkers/util/testing/DefaultSamplers.scala
@@ -35,82 +35,14 @@ import org.joda.time.{DateTime, DateTimeZone, LocalDate}
 import org.scalacheck.Gen
 import com.outworkers.util.domain._
 
+import scala.util.Random
+
 trait Sample[T] {
   def sample: T
 }
 
-object Sample {
-  def apply[T : Sample]: Sample[T] = implicitly[Sample[T]]
-}
+object Sample extends Generators {
 
-sealed trait Generators extends GenerationDomain {
-
-  protected[this] val domains = List("net", "com", "org", "io", "biz", "co.uk", "co.za")
-  protected[this] val protocols = List("http", "https")
-
-  val defaultGenerationSize = 5
-
-  /**
-   * Uses the type class available in implicit scope to mock a certain custom object.
-   * @tparam T The parameter to mock.
-   * @return A sample of the given type generated using the implicit sampler.
-   */
-  def gen[T : Sample]: T = implicitly[Sample[T]].sample
-
-  /**
-   * Generates a tuple of the given type arguments, using the implicit samplers in scope.
-   * @tparam X The first type of the tuple to be sampled.
-   * @tparam Y The second type of the type to be sampled.
-   * @return A Tuple2[X, Y] generated using the implicit samplers.
-   */
-  def gen[X: Sample, Y: Sample]: (X, Y) = (gen[X], gen[Y])
-
-  def genOpt[T : Sample]: Option[T] = Some(implicitly[Sample[T]].sample)
-
-  def genList[T : Sample](size: Int = defaultGenerationSize): List[T] = List.tabulate(size)(i => gen[T])
-
-  def genMap[T: Sample](size: Int = defaultGenerationSize): Map[String, T] = {
-    genList[T](size).map(item => (item.toString, item)).toMap
-  }
-
-  /**
-   * Generates a map of known key -> value types using implicit samplers.
-   * @param size The number of elements to generate in the map.
-   * @tparam Key The type of the key the generated map should have. Needs a Sample[Key] in scope.
-   * @tparam Value The type of the value the generated map should have. Needs a Sample[Value] in scope.
-   * @return A key -> value map generated using the pre-defined samples for Key and Value.
-   */
-  def genMap[Key : Sample, Value : Sample](size: Int): Map[Key, Value] = {
-    List.tabulate(size)(i => (gen[Key], gen[Value])).toMap
-  }
-
-  /**
-   * Generates a map using a Sampler for the key and a function Key -> Value for the value.
-   * Useful when the value of a key can be inferred by knowing the key itself.
-   *
-   * The implementation uses the value during mapping as the genMap function called with
-   * a single type argument will generate a Map[String, Type].
-   *
-   * @param size The size of the map to generate.
-   * @param producer The function used to generate the value from a key.
-   * @tparam Key The type of the Key to generate, needs to have a Sample available in scope.
-   * @tparam Value The type of the Value to generate.
-   * @return A map of the given size with sampled keys and values inferred by the producer function.
-   */
-  def genMap[Key : Sample, Value](size: Int, producer: Key => Value): Map[Key, Value] = {
-    genMap[Key](size) map {
-      case (k, v) => (v, producer(v))
-    }
-  }
-
-  def oneOf[T](list: Seq[T]): T = Gen.oneOf(list).sample.get
-
-  def oneOf[T <: Enumeration](enum: T): T#Value = {
-    oneOf(enum.values.toList)
-  }
-}
-
-trait DefaultSamplers extends Generators {
 
   implicit object StringSample extends Sample[String] {
     def sample: String = Sampler.string
@@ -120,6 +52,10 @@ trait DefaultSamplers extends Generators {
     def sample: ShortString = {
       ShortString(java.lang.Long.toHexString(java.lang.Double.doubleToLongBits(Math.random())))
     }
+  }
+
+  implicit object BooleanSampler extends Sample[Boolean] {
+    def sample: Boolean = Random.nextBoolean()
   }
 
   implicit object IntSample extends Sample[Int] {
@@ -203,6 +139,73 @@ trait DefaultSamplers extends Generators {
       oneOf(protocols) + "://www." + gen[ShortString].str + "." + oneOf(domains)
     )
   }
+
+  def apply[T : Sample]: Sample[T] = implicitly[Sample[T]]
 }
 
+private[util] trait Generators extends GenerationDomain {
 
+  protected[this] val domains = List("net", "com", "org", "io", "biz", "co.uk", "co.za")
+  protected[this] val protocols = List("http", "https")
+
+  val defaultGenerationSize = 5
+
+  /**
+   * Uses the type class available in implicit scope to mock a certain custom object.
+   * @tparam T The parameter to mock.
+   * @return A sample of the given type generated using the implicit sampler.
+   */
+  def gen[T : Sample]: T = implicitly[Sample[T]].sample
+
+  /**
+   * Generates a tuple of the given type arguments, using the implicit samplers in scope.
+   * @tparam X The first type of the tuple to be sampled.
+   * @tparam Y The second type of the type to be sampled.
+   * @return A Tuple2[X, Y] generated using the implicit samplers.
+   */
+  def gen[X: Sample, Y: Sample]: (X, Y) = (gen[X], gen[Y])
+
+  def genOpt[T : Sample]: Option[T] = Some(implicitly[Sample[T]].sample)
+
+  def genList[T : Sample](size: Int = defaultGenerationSize): List[T] = List.tabulate(size)(i => gen[T])
+
+  def genMap[T : Sample](size: Int = defaultGenerationSize): Map[String, T] = {
+    genList[T](size).map(item => (item.toString, item)).toMap
+  }
+
+  /**
+   * Generates a map of known key -> value types using implicit samplers.
+   * @param size The number of elements to generate in the map.
+   * @tparam Key The type of the key the generated map should have. Needs a Sample[Key] in scope.
+   * @tparam Value The type of the value the generated map should have. Needs a Sample[Value] in scope.
+   * @return A key -> value map generated using the pre-defined samples for Key and Value.
+   */
+  def genMap[Key : Sample, Value : Sample](size: Int): Map[Key, Value] = {
+    List.tabulate(size)(i => (gen[Key], gen[Value])).toMap
+  }
+
+  /**
+   * Generates a map using a Sampler for the key and a function Key -> Value for the value.
+   * Useful when the value of a key can be inferred by knowing the key itself.
+   *
+   * The implementation uses the value during mapping as the genMap function called with
+   * a single type argument will generate a Map[String, Type].
+   *
+   * @param size The size of the map to generate.
+   * @param producer The function used to generate the value from a key.
+   * @tparam Key The type of the Key to generate, needs to have a Sample available in scope.
+   * @tparam Value The type of the Value to generate.
+   * @return A map of the given size with sampled keys and values inferred by the producer function.
+   */
+  def genMap[Key : Sample, Value](size: Int, producer: Key => Value): Map[Key, Value] = {
+    genMap[Key](size) map {
+      case (k, v) => (v, producer(v))
+    }
+  }
+
+  def oneOf[T](list: Seq[T]): T = Gen.oneOf(list).sample.get
+
+  def oneOf[T <: Enumeration](enum: T): T#Value = {
+    oneOf(enum.values.toList)
+  }
+}
--- util-testing/src/main/scala/com/outworkers/util/testing/SamplerMacro.scala
+++ util-testing/src/main/scala/com/outworkers/util/testing/SamplerMacro.scala
@@ -5,6 +5,8 @@ class SamplerMacro(val c: scala.reflect.macros.blackbox.Context) {
 
   import c.universe._
 
+  val prefix = q"com.outworkers.util.testing"
+
   /**
     * Retrieves the accessor fields on a case class and returns an iterable of tuples of the form Name -> Type.
     * For every single field in a case class, a reference to the string name and string type of the field are returned.
@@ -21,14 +23,118 @@ class SamplerMacro(val c: scala.reflect.macros.blackbox.Context) {
     * @return An iterable of tuples where each tuple encodes the string name and string type of a field.
     */
   def accessors(
-    params: Seq[c.universe.ValDef]
-  ): Iterable[(c.universe.TermName, c.universe.TypeName)] = {
+    params: Seq[ValDef]
+  ): Iterable[(TermName, TypeName)] = {
     params.map {
       case ValDef(_, name: TermName, tpt: Tree, _) => name -> TypeName(tpt.toString)
     }
   }
 
-  val prefix = q"com.outworkers.util.testing"
+  def fieldNameInference(nm: TermName): Option[TypeName] = {
+    case "first_name" | "firstname" => Some(tq"$prefix.FirstName")
+    case "last_name" | "lastname" => Some(tq"$prefix.LastName")
+    case "name" | "fullname" | "full_name" => Some(tq"""$prefix.FullName""")
+    case "email" | "email_address" | "emailaddress" => Some(tq"""$prefix.EmailAddress""")
+    case "country" => Some(tq"""$prefix.CountryCode""")
+    case _ => None
+  }
+
+  object KnownField {
+    def unapply(arg: TermName): Option[TypeName] = fieldNameInference(arg)
+  }
+
+  /**
+    * Describes a complex collection type.
+    * The source types are the type arguments that will be needed to produce the collection.
+    * We have more than one because in the case of a Map for instance we could have multiple ones.
+    *
+    * The applier function is a way to produce a new type of the same collection type given an input type.
+    * It's useful because we often want to derive the types of the generators based on the name of the field,
+    * to offer the auto-replacement funtionality for collections.
+    *
+    * @param sources The source types that are needed to produce the collection type.
+    * @param applier The applier function that allows producing a collection type from a list of type arguments.
+    */
+  case class CollectionType(
+    sources: List[TypeName],
+    applier: List[TypeName] => TypeName
+  )
+
+  object CollectionType {
+    def apply(
+      source: TypeName,
+      applier: List[TypeName] => TypeName
+    ): Option[CollectionType] = Some(new CollectionType(source :: Nil, applier))
+
+    def unapply(arg: TypeName): Option[CollectionType] = collectionType(arg)
+  }
+
+
+  private[this] def collectionType(
+    source: TypeName
+  ): Option[CollectionType] = {
+    val strTpe = source.decodedName.toString
+
+    if (strTpe.startsWith("List[")) {
+      CollectionType.apply(
+        source = TypeName(strTpe.drop(5).dropRight(1)),
+        applier = applied => TypeName(s"scala.collection.immutable.List[..$applied]")
+      )
+    } else if (strTpe.startsWith("Option[")) {
+      CollectionType(
+        source = TypeName(strTpe.drop(4).dropRight(1)),
+        applier = applied => TypeName(s"scala.Option[$applied]")
+      )
+    } else if (strTpe.startsWith("Set[")) {
+      CollectionType(
+        source = TypeName(strTpe.drop(4).dropRight(1)),
+        applied => TypeName(s"scala.collection.immutable.Set[..$applied]")
+      )
+    } else if (strTpe.startsWith("Map[")) {
+      val parts = strTpe.replaceAll(" ", "").drop(4).split(",")
+
+      Some(
+        CollectionType(
+          List(TypeName(parts(0)), TypeName(parts(1))),
+          applied => TypeName(s"scala.collection.immutable.Map[..$applied]")
+        )
+      )
+    } else {
+      None
+    }
+  }
+
+  object CollectionSample {
+    def unapply(arg: TypeName): Option[CollectionType] = collectionType(arg)
+  }
+
+  private[this] def deriveSamplerType(
+    field: TermName,
+    tpe: TypeName
+  ): TypeName = {
+    val strTpe = tpe.decodedName.toString
+
+    tpe match {
+      case col @ CollectionType(tp) => {
+
+      }
+    }
+
+
+
+    if (strTpe.startsWith("List[")) {
+      tq"$prefix.genList[${TypeName(strTpe.drop(5).dropRight(1))}]()"
+    } else if(strTpe.startsWith("Option[")) {
+      tq"$prefix.genOpt[${TypeName(strTpe.drop(4).dropRight(1))}]()"
+    } else if (strTpe.startsWith("Set[")) {
+      tq"$prefix.getSet[${TypeName(strTpe.drop(4).dropRight(1))}]()"
+    } else if (strTpe.startsWith("Map[")) {
+      val parts = strTpe.replaceAll(" ", "").drop(4).split(",")
+      tq"$prefix.genMap[${TypeName(parts(0))}, ${TypeName(parts(1))}]()"
+    } else {
+      tq"$tpe"
+    }
+  }
 
   def inferType(nm: TermName, tp: TypeName): Tree = {
     val str = nm.decodedName.toString
