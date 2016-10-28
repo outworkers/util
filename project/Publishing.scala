54b11aba4b00bb7b8d00188c6066300e3c030a6d
Flavian Alexandru <flavian@outworkers.com>
2016-10-28 20:10:37 +0100

    Adding a snapshot target repo

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -94,8 +94,10 @@ object Publishing {
   val bintraySettings: Seq[Def.Setting[_]] = Seq(
     publishMavenStyle := true,
     bintrayReleaseOnPublish in ThisBuild := true,
-    bintrayOrganization := Some("websudos"),
-    bintrayRepository := "oss-releases",
+    bintrayOrganization := Some("outworkers"),
+    bintrayRepository <<= scalaVersion.apply {
+      v => if (v.trim.endsWith("SNAPSHOT")) "oss-snapshots" else "oss-releases"
+    },
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => true},
     licenses += ("Apache-2.0", url("https://www.apache.org/licenses/LICENSE-2.0"))
