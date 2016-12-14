c9e7541196f474ea6112e07e9b93ef3bc8739226
Flavian Alexandru <flavian@outworkers.com>
2016-12-14 11:31:28 +0000

    Replacing command alias

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -2,10 +2,10 @@
 if [ "${TRAVIS_SCALA_VERSION}" == "2.12.0" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
 then
     echo "Running tests with coverage and report submission"
-    sbt "+++$TRAVIS_SCALA_VERSION test"
+    sbt "plz $TRAVIS_SCALA_VERSION test"
     exit $?
 else
     echo "Running tests without attempting to submit coverage reports"
-    sbt "+++$TRAVIS_SCALA_VERSION test"
+    sbt "plz $TRAVIS_SCALA_VERSION test"
     exit $?
 fi
