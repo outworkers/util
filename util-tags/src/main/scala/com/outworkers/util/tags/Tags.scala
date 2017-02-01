7528f0874e1044769ca3d8c0b85fbc52182d3fb3
Flavian Alexandru <flavian@outworkers.com>
2017-02-01 17:39:57 +0000

    Using lowercase names

--- util-tags/src/main/scala/com/outworkers/util/tags/Tags.scala
+++ util-tags/src/main/scala/com/outworkers/util/tags/Tags.scala
@@ -11,50 +11,49 @@ class Tags(val c: blackbox.Context) {
 
   def tagMacro[A: c.WeakTypeTag, T: c.WeakTypeTag]: c.Expr[Tags.Aux[A, T]] = {
     import c.universe._
-    val AT = weakTypeOf[Tags.Aux[A, T]]
+    val aTpe = weakTypeOf[Tags.Aux[A, T]]
     val a = c.prefix.tree match {
       case Apply(_, List(x)) => x
       case t => c.abort(c.enclosingPosition, s"Cannot extract .tag target (tree = $t)")
     }
-    c.Expr[Tags.Aux[A, T]](q"$a.asInstanceOf[$AT]")
+    c.Expr[Tags.Aux[A, T]](q"$a.asInstanceOf[$aTpe]")
   }
 
   def untagMacro[A: c.WeakTypeTag, T: c.WeakTypeTag]: c.Expr[A] = {
     import c.universe._
-    val A = weakTypeOf[A]
+    val aTpe = weakTypeOf[A]
     val at = c.prefix.tree match {
       case Apply(_, List(x)) => x
       case t => c.abort(c.enclosingPosition, s"Cannot extract .untag target (tree = $t)")
     }
-    c.Expr[A](q"$at.asInstanceOf[$A]")
+    c.Expr[A](q"$at.asInstanceOf[$aTpe]")
   }
 
   def wrapMacro[A: c.WeakTypeTag, T: c.WeakTypeTag](a: c.Expr[A]): c.Expr[Tags.Aux[A, T]] = {
-    val A = weakTypeOf[A]
-    val T = weakTypeOf[T]
-    val AT = weakTypeOf[Tags.Aux[A, T]]
-    c.Expr[Tags.Aux[A, T]](q"$a.asInstanceOf[$pkg.Tags.Aux[$A, $T]]")
+    val aTpe = weakTypeOf[A]
+    val tTpe = weakTypeOf[T]
+    c.Expr[Tags.Aux[A, T]](q"$a.asInstanceOf[$pkg.Tags.Aux[$aTpe, $tTpe]]")
   }
 
   def unwrapMacro[A: c.WeakTypeTag, T](at: c.Expr[Tags.Aux[A, T]]): c.Expr[A] = {
-    val A = weakTypeOf[A]
-    c.Expr[A](q"$at.asInstanceOf[$A]")
+    val aTpe = weakTypeOf[A]
+    c.Expr[A](q"$at.asInstanceOf[$aTpe]")
   }
 
   def wrapfMacro[F[_], A: c.WeakTypeTag, T: c.WeakTypeTag](fa: c.Expr[F[A]])(
-    implicit F: c.WeakTypeTag[F[_]]
+    implicit fTag: c.WeakTypeTag[F[_]]
   ): c.Expr[F[Tags.Aux[A, T]]] = {
-    val AT = appliedType(typeOf[Tags.Aux[_, _]], List(weakTypeOf[A], weakTypeOf[T]))
-    val FAT = appliedType(F.tpe.typeConstructor, AT :: Nil)
-    val t = q"$fa.asInstanceOf[$FAT]"
+    val aTpe = appliedType(typeOf[Tags.Aux[_, _]], List(weakTypeOf[A], weakTypeOf[T]))
+    val faTpe = appliedType(fTag.tpe.typeConstructor, aTpe :: Nil)
+    val t = q"$fa.asInstanceOf[$faTpe]"
     c.Expr[F[Tags.Aux[A, T]]](t)
   }
 
   def unwrapfMacro[F[_], A: c.WeakTypeTag, T](fat: c.Expr[F[Tags.Aux[A, T]]])(
-    implicit F: c.WeakTypeTag[F[_]]
+    implicit fTag: c.WeakTypeTag[F[_]]
   ): c.Expr[F[A]] = {
-    val FA = appliedType(F.tpe.typeConstructor, weakTypeOf[A] :: Nil)
-    c.Expr[F[A]](q"$fat.asInstanceOf[$FA]")
+    val faTpe = appliedType(fTag.tpe.typeConstructor, weakTypeOf[A] :: Nil)
+    c.Expr[F[A]](q"$fat.asInstanceOf[$faTpe]")
   }
 }
 
