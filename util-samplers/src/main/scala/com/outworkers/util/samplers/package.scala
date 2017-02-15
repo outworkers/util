2bc7d1c709915a2971b2737eb488ea67b5101963
Flavian Alexandru <alexflav23@users.noreply.github.com>
2017-02-15 17:13:12 +0000

    Removing tag dependencies (#51)

--- util-samplers/src/main/scala/com/outworkers/util/samplers/package.scala
+++ util-samplers/src/main/scala/com/outworkers/util/samplers/package.scala
@@ -1,9 +1,8 @@
 package com.outworkers.util
 
 import com.outworkers.util.domain.GenerationDomain
-import com.outworkers.util.tags.DefaultTaggedTypes
 
-package object samplers extends Generators with GenerationDomain with DefaultTaggedTypes {
+package object samplers extends Generators with GenerationDomain {
 
   implicit class Printer[T](val obj: T) extends AnyVal {
     def trace()(implicit tracer: Tracer[T]): String = tracer.trace(obj)
