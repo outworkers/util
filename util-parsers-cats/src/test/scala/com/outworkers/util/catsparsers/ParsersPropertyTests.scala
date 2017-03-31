a81fc8fc5bd9ea73c3ec18d6e1d061aeab13a474
Flavian Alexandru <flavian@outworkers.com>
2017-03-31 15:38:22 +0100

    Adding more tests to property parsing

--- util-parsers-cats/src/test/scala/com/outworkers/util/catsparsers/ParsersPropertyTests.scala
+++ util-parsers-cats/src/test/scala/com/outworkers/util/catsparsers/ParsersPropertyTests.scala
@@ -2,8 +2,12 @@ package com.outworkers.util.catsparsers
 
 import org.scalatest.{FlatSpec, Matchers, OptionValues}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import com.outworkers.util.testing._
+import org.joda.time.DateTime
 
 class ParsersPropertyTests extends FlatSpec with Matchers with OptionValues with GeneratorDrivenPropertyChecks {
+
+  implicit val dateTimeGen = Sample.arbitrary[DateTime]
 
   it should "parse a long from a string value for every generated long" in {
     forAll { value: Long =>
@@ -12,7 +16,22 @@ class ParsersPropertyTests extends FlatSpec with Matchers with OptionValues with
 
       parsed.toOption.value shouldEqual value
     }
+  }
 
+  it should "parse a DateTime from a valid millisecond string" in {
+    forAll { value: DateTime =>
+      val parsed = parse[DateTime](value.getMillis.toString)
+      parsed.isValid shouldEqual true
+      parsed.toOption.value shouldEqual value
+    }
+  }
+
+  it should "parse a DateTime from a valid millisecond string using biparse" in {
+    forAll { value: DateTime =>
+      val parsed = biparse[String, DateTime](value.getMillis.toString)
+      parsed.isValid shouldEqual true
+      parsed.toOption.value shouldEqual value
+    }
   }
 
 }
