a7a851afbd167dcec228b1c2163dc01bd756ac8f
Flavian Alexandru <flavian@outworkers.com>
2017-05-28 19:02:06 +0100

    Adding more tests

--- util-parsers-cats/src/test/scala/com/outworkers/util/parsers/ParsersPropertyTests.scala
+++ util-parsers-cats/src/test/scala/com/outworkers/util/parsers/ParsersPropertyTests.scala
@@ -13,6 +13,22 @@ class ParsersPropertyTests extends FlatSpec with Matchers with OptionValues with
   implicit val dateTimeGen = Sample.arbitrary[DateTime]
   implicit val uuidGen = Sample.arbitrary[UUID]
 
+  def parserOptTest[T : Parser : Arbitrary](fn: T => String): Assertion = {
+    forAll { value: T =>
+      val parsed = parse[T](Some(fn(value)))
+      parsed.isValid shouldEqual true
+      parsed.toOption.value shouldEqual value
+    }
+  }
+
+  def validateOptTest[T : Parser : Arbitrary](fn: T => String): Assertion = {
+    forAll { value: T =>
+      val parsed = validate[T](Some(fn(value)))
+      parsed.isValid shouldEqual true
+      parsed.toOption.value shouldEqual value
+    }
+  }
+
   def parserTest[T : Parser : Arbitrary](fn: T => String): Assertion = {
     forAll { value: T =>
       val parsed = parse[T](fn(value))
@@ -57,14 +73,6 @@ class ParsersPropertyTests extends FlatSpec with Matchers with OptionValues with
     parserTest[DateTime](_.getMillis.toString)
   }
 
-  it should "parse a DateTime from a valid millisecond string using biparse" in {
-    forAll { value: DateTime =>
-      val parsed = biparse[String, DateTime](value.getMillis.toString)
-      parsed.isValid shouldEqual true
-      parsed.toOption.value shouldEqual value
-    }
-  }
-
   it should "validate a long from a string value for every generated long" in {
     validateTest[Long](_.toString)
   }
@@ -91,5 +99,69 @@ class ParsersPropertyTests extends FlatSpec with Matchers with OptionValues with
 
   it should "validate a DateTime from a valid millisecond string" in {
     validateTest[DateTime](_.getMillis.toString)
+  }
+
+  it should "validate a Option[Boolean] value from a string" in {
+    validateOptTest[Boolean](_.toString)
+  }
+
+  it should "validate a Option[Float] value from a string" in {
+    validateOptTest[Float](_.toString)
+  }
+
+  it should "validate an Option[UUID] value from a string" in {
+    validateOptTest[UUID](_.toString)
+  }
+
+  it should "validate a Option[Double] value from a string" in {
+    validateOptTest[Double](_.toString)
+  }
+
+  it should "validate an Option[Int] value from a string" in {
+    validateOptTest[Int](_.toString)
+  }
+
+  it should "validate a Option[DateTime] from a valid millisecond string" in {
+    validateOptTest[DateTime](_.getMillis.toString)
+  }
+
+  it should "validate a Option[DateTime] from a valid millisecond string using biparse" in {
+    forAll { value: DateTime =>
+      val parsed = validate[DateTime](Some(value.getMillis.toString))
+      parsed.isValid shouldEqual true
+      parsed.toOption.value shouldEqual value
+    }
+  }
+
+  it should "parse a Option[Boolean] value from a string" in {
+    parserOptTest[Boolean](_.toString)
+  }
+
+  it should "parse a Option[Float] value from a string" in {
+    parserOptTest[Float](_.toString)
+  }
+
+  it should "parse an Option[UUID] value from a string" in {
+    parserOptTest[UUID](_.toString)
+  }
+
+  it should "parse a Option[Double] value from a string" in {
+    parserOptTest[Double](_.toString)
+  }
+
+  it should "parse an Option[Int] value from a string" in {
+    parserOptTest[Int](_.toString)
+  }
+
+  it should "parse a Option[DateTime] from a valid millisecond string" in {
+    parserOptTest[DateTime](_.getMillis.toString)
+  }
+
+  it should "parse a Option[DateTime] from a valid millisecond string using biparse" in {
+    forAll { value: DateTime =>
+      val parsed = parse[DateTime](Some(value.getMillis.toString))
+      parsed.isValid shouldEqual true
+      parsed.toOption.value shouldEqual value
+    }
   }
 }
