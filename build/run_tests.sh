6ff15ef43d8d94c9a832ca8b1f1a8e1585726e69
Flavian Alexandru <flavian@outworkers.com>
2018-03-16 18:20:34 +0000

    Fixing scala versions.

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-if [ "${TRAVIS_SCALA_VERSION}" == "2.11.8" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
+if [ "${TRAVIS_SCALA_VERSION}" == "2.12.4" ] && [ "${TRAVIS_JDK_VERSION}" == "oraclejdk8" ];
 then
     echo "Running tests with coverage and report submission"
     sbt ++$TRAVIS_SCALA_VERSION coverage test coverageReport coverageAggregate
-    exit $?
 else
     echo "Running tests without attempting to submit coverage reports"
     sbt "plz $TRAVIS_SCALA_VERSION test"
-    exit $?
 fi
-q
