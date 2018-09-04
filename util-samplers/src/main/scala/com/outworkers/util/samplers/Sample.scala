ab76f571a25b4eda03a7c2ed8e911daac4c285c5
Flavian Alexandru <flavian@outworkers.com>
2018-09-04 15:15:05 +0100

    Improving samplers and removing need to have nested field names

--- util-samplers/src/main/scala/com/outworkers/util/samplers/Sample.scala
+++ util-samplers/src/main/scala/com/outworkers/util/samplers/Sample.scala
@@ -20,32 +20,22 @@ import java.util.{Date, Locale, UUID}
 
 import org.scalacheck.{Arbitrary, Gen}
 
+import scala.annotation.implicitNotFound
 import scala.collection.generic.CanBuildFrom
 import scala.util.Random
 
+@implicitNotFound("No automated way to create A Sample for ${T}. Create an implicit Sample for ${T} in scope manually")
 trait Sample[T] {
   def sample: T
 }
 
 object Sample extends Generators {
 
-  def iso[T : Sample, T1](fn: T => T1): Sample[T1] = derive(fn)
 
-  def derive[T : Sample, T1](fn: T => T1): Sample[T1] = new Sample[T1] {
-    override def sample: T1 = fn(gen[T])
-  }
 
   def arbitrary[T : Sample]: Arbitrary[T] = Arbitrary(generator[T])
 
   def generator[T : Sample]: Gen[T] = Gen.delay(gen[T])
-
-  /**
-    * !! Warning !! Black magic going on. This will use the excellent macro compat
-    * library to macro materialise an instance of the required primitive based on the type argument.
-    * @tparam T The type parameter to materialise a sample for.
-    * @return A derived sampler, materialised via implicit blackbox macros.
-    */
-  implicit def materialize[T]: Sample[T] = macro SamplerMacro.materialize[T]
 
   def collection[M[X] <: TraversableOnce[X], T : Sample](
     implicit cbf: CanBuildFrom[Nothing, T, M[T]]
@@ -190,4 +180,24 @@ object Sample extends Generators {
     }
   }
 
+  def iso[T : Sample, T1](fn: T => T1): Sample[T1] = derive(fn)
+
+  /**
+    * Derives samplers and encodings for a non standard type.
+    * @param fn The function that converts a [[T]] instance to a [[T1]] instance.
+    * @tparam T1 The type you want to derive a sampler for.
+    * @tparam T The source type of the sampler, must already have a sampler defined for it.
+    * @return A new sampler that can interact with the target type.
+    */
+  def derive[T : Sample, T1](fn: T => T1): Sample[T1] = new Sample[T1] {
+    override def sample: T1 = fn(gen[T])
+  }
+
+  /**
+    * !! Warning !! Black magic going on. This will use the excellent macro compat
+    * library to macro materialise an instance of the required primitive based on the type argument.
+    * @tparam T The type parameter to materialise a sample for.
+    * @return A derived sampler, materialised via implicit blackbox macros.
+    */
+  implicit def materialize[T]: Sample[T] = macro SamplerMacro.materialize[T]
 }
