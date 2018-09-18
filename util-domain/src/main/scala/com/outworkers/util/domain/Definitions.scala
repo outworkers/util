6d430546968516a81864f953719ff242d26f3d9b
Flavian Alexandru <alexflav23@users.noreply.github.com>
2018-09-18 12:24:16 +0100

    Re-adding a trait to have alias definitions (#70)

---
+++ util-domain/src/main/scala/com/outworkers/util/domain/Definitions.scala
@@ -0,0 +1,18 @@
+package com.outworkers.util.domain
+
+import com.outworkers.util.domain
+
+trait Definitions {
+  type EmailAddress = domain.EmailAddress
+  type City = domain.City
+  type Country = domain.Country
+  type CountryCode = domain.CountryCode
+  type Domain = domain.Domain
+  type FirstName = domain.FirstName
+  type FullName = domain.FullName
+  type LastName = domain.LastName
+  type LoremIpsum = domain.LoremIpsum
+  type ProgrammingLanguage = domain.ProgrammingLanguage
+  type ShortString = domain.ShortString
+  type Url = domain.Url
+}
