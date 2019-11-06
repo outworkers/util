933e46d3bcb7d50cd4cf85157be5fdef734e85db
Flavian Alexandru <flavian@outworkers.com>
2019-11-06 02:08:33 +0000

    Changing year

--- project/Publishing.scala
+++ project/Publishing.scala
@@ -13,11 +13,15 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+import bintray.BintrayKeys._
+import com.typesafe.sbt.SbtGit.git
 import sbt.Keys._
 import sbt._
-import com.typesafe.sbt.pgp.PgpKeys._
+import com.jsuereth.sbtpgp.PgpKeys._
 import sbtrelease.ReleasePlugin.autoImport.{ReleaseStep, _}
+import sbtrelease.ReleaseStateTransformations._
 import sbtrelease.Vcs
+
 import scala.util.Properties
 
 object Publishing {
@@ -30,90 +34,112 @@ object Publishing {
 
   val ciSkipSequence = "[ci skip]"
 
-  private def toProcessLogger(st: State): ProcessLogger = new ProcessLogger {
-    override def error(s: => String): Unit = st.log.error(s)
-    override def info(s: => String): Unit = st.log.info(s)
-    override def buffer[T](f: => T): T = st.log.buffer(f)
-  }
-
   def vcs(state: State): Vcs = {
     Project.extract(state).get(releaseVcs)
       .getOrElse(sys.error("Aborting release. Working directory is not a repository of a recognized VCS."))
   }
 
   val releaseTutFolder = settingKey[File]("The file to write the version to")
-  val releaseTutCommit = taskKey[String]("Commit message for the tut commit")
 
   def commitTutFilesAndVersion: ReleaseStep = ReleaseStep { st: State =>
     val settings = Project.extract(st)
-    val logger = ConsoleLogger()
-    logger.info(s"Found modified files: ${vcs(st).hasModifiedFiles}")
-
-    val log = toProcessLogger(st)
     val versionsFile = settings.get(releaseVersionFile).getCanonicalFile
     val docsFolder = settings.get(releaseTutFolder).getCanonicalFile
-
-    logger.info(s"Docs folder path: Path: ${docsFolder.getPath}; Absolute path: ${docsFolder.getAbsolutePath}")
     val base = vcs(st).baseDir.getCanonicalFile
     val sign = settings.get(releaseVcsSign)
 
-    val versionPath = IO.relativize(
+    val relativePath = IO.relativize(
       base,
       versionsFile
     ).getOrElse("Version file [%s] is outside of this VCS repository with base directory [%s]!" format(versionsFile, base))
 
-    val commitablePaths = Seq(versionPath) ++ {
-      if (docsFolder.exists) {
-        logger.info(s"Docs folder exists under $docsFolder")
-        val relativeDocsPath = IO.relativize(
-          base,
-          docsFolder
-        ).getOrElse("Docs folder [%s] is outside of this VCS repository with base directory [%s]!" format(docsFolder, base))
-        Seq(relativeDocsPath)
-      } else {
-        logger.info(s"Docs folder doesn't exist under, $base and $docsFolder")
-        Seq.empty
-      }
-    }
+    val relativeDocsPath = IO.relativize(
+      base,
+      docsFolder
+    ).getOrElse("Docs folder [%s] is outside of this VCS repository with base directory [%s]!" format(docsFolder, base))
 
-    vcs(st).add(commitablePaths: _*) !! log
+    vcs(st).add(relativePath) !! st.log
+    vcs(st).add(relativeDocsPath) !! st.log
     val status = (vcs(st).status !!) trim
 
     val newState = if (status.nonEmpty) {
       val (state, msg) = settings.runTask(releaseCommitMessage, st)
-      val x = vcs(state).commit(msg, sign)
-
+      vcs(state).commit(msg, sign, false) ! st.log
       state
     } else {
-      // nothing to commit. this happens if the version.sbt file hasn't changed or no docs have been added.
+      // nothing to commit. this happens if the version.sbt file hasn't changed.
       st
     }
-    vcs(newState).status !! log
-
     newState
   }
 
-  lazy val defaultCredentials: Seq[Credentials] = {
-    Seq(
-      Credentials(Path.userHome / ".ivy2" / ".credentials"),
-      Credentials(
-        realm = "Sonatype OSS Repository Manager",
-        host = "oss.sonatype.org",
-        userName = System.getenv("maven_user"),
-        passwd = System.getenv("maven_password")
-      )
+  val releaseSettings = Seq(
+    releaseTutFolder in ThisBuild := baseDirectory.value / "docs",
+    releaseIgnoreUntrackedFiles := true,
+    releaseVersionBump := sbtrelease.Version.Bump.Minor,
+    releaseTagComment := s"Releasing ${(version in ThisBuild).value} $ciSkipSequence",
+    releaseCommitMessage := s"Setting version to ${(version in ThisBuild).value} $ciSkipSequence",
+    releaseProcess := Seq[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      releaseStepCommandAndRemaining("+publishSigned"),
+      releaseStepCommandAndRemaining("sonatypeReleaseAll"),
+      setNextVersion,
+      commitNextVersion,
+      pushChanges
     )
+  )
+
+  lazy val defaultCredentials: Seq[Credentials] = {
+    if (!Publishing.runningUnderCi) {
+      Seq(
+        Credentials(Path.userHome / ".bintray" / ".credentials"),
+        Credentials(Path.userHome / ".ivy2" / ".credentials")
+      )
+    } else {
+      Seq(
+        Credentials(
+          realm = "Bintray",
+          host = "dl.bintray.com",
+          userName = System.getenv("bintray_user"),
+          passwd = System.getenv("bintray_password")
+        ),
+        Credentials(
+          realm = "Sonatype OSS Repository Manager",
+          host = "oss.sonatype.org",
+          userName = System.getenv("maven_user"),
+          passwd = System.getenv("maven_password")
+        ),
+        Credentials(
+          realm = "Bintray API Realm",
+          host = "api.bintray.com",
+          userName = System.getenv("bintray_user"),
+          passwd = System.getenv("bintray_password")
+        )
+      )
+    }
   }
 
-  def publishToMaven: Boolean = sys.env.get("MAVEN_PUBLISH").exists("true" ==)
-
-  lazy val pgpPass: Option[Array[Char]] = Properties.envOrNone("pgp_passphrase")
-    .orElse(Properties.envOrNone("PGP_PASSPHRASE")).map(_.toCharArray)
+  lazy val pgpPass: Option[Array[Char]] = Properties.envOrNone("pgp_passphrase").map(_.toCharArray)
 
   lazy val mavenSettings: Seq[Def.Setting[_]] = Seq(
     credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
     publishMavenStyle := true,
-    licenses += ("Apache-2.0", url("https://github.com/outworkers/phantom/blob/develop/LICENSE.txt")),
+    Global / pgpPassphrase := {
+      if (runningUnderCi && pgpPass.isDefined) {
+        println("Running under CI and PGP password specified under settings.")
+        println(s"Password longer than five characters: ${pgpPass.exists(_.length > 5)}")
+        pgpPass
+      } else {
+        println("Could not find settings for a PGP passphrase.")
+        println(s"pgpPass defined in environemnt: ${pgpPass.isDefined}")
+        println(s"Running under CI: $runningUnderCi")
+        None
+      }
+    },
     publishTo := {
       val nexus = "https://oss.sonatype.org/"
       if (version.value.trim.endsWith("SNAPSHOT")) {
@@ -122,14 +148,14 @@ object Publishing {
         Some("releases" at nexus + "service/local/staging/deploy/maven2")
       }
     },
-    externalResolvers := Resolver.withDefaultResolvers(resolvers.value, mavenCentral = true),
+    licenses += ("Outworkers License", url("https://github.com/outworkers/phantom/blob/develop/LICENSE.txt")),
     publishArtifact in Test := false,
     pomIncludeRepository := { _ => true },
     pomExtra :=
       <url>https://github.com/outworkers/phantom</url>
         <scm>
-          <url>git@github.com:outworkers/util.git</url>
-          <connection>scm:git:git@github.com:outworkers/util.git</connection>
+          <url>git@github.com:outworkers/phantom.git</url>
+          <connection>scm:git:git@github.com:outworkers/phantom.git</connection>
         </scm>
         <developers>
           <developer>
@@ -142,12 +168,23 @@ object Publishing {
 
   def effectiveSettings: Seq[Def.Setting[_]] = mavenSettings
 
+  /**
+    * This exists because SBT is not capable of reloading publishing configuration during tasks or commands.
+    * Unfortunately we have to load a specific configuration based on an environment variable that we "flip"
+    * during CI.
+    */
+  def publishingToMaven: Boolean = {
+    sys.env.exists { case (k, v) => k.equalsIgnoreCase("MAVEN_PUBLISH") && v.equalsIgnoreCase("true") }
+  }
+
   def runningUnderCi: Boolean = sys.env.get("CI").isDefined || sys.env.get("TRAVIS").isDefined
   def travisScala211: Boolean = sys.env.get("TRAVIS_SCALA_VERSION").exists(_.contains("2.11"))
 
   def isTravisScala210: Boolean = !travisScala211
 
   def isJdk8: Boolean = sys.props("java.specification.version") == "1.8"
+
+  def jdk8Only(ref: ProjectReference): Seq[ProjectReference] = addOnCondition(isJdk8, ref)
 
   lazy val addOnCondition: (Boolean, ProjectReference) => Seq[ProjectReference] = (bool, ref) =>
     if (bool) ref :: Nil else Nil
